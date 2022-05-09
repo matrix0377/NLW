24d766d90dc7151d927008263df87bf40aeb605f
Davi Augusto <droidcenter0377@gmail.com>
2022-05-09 14:16:17 -0300

    atualização

---
+++ main.js
@@ -0,0 +1,22 @@
+// Continur o video em 1:43:21 -->> Stage 2
+
+// String (textos)
+// Number (numeros)
+// Boolean (True | False)
+
+function onScroll() {
+    if (scrollY > 0) {
+        navigation.classList.add('scroll')
+    } else {
+        navigation.classList.remove('scroll')
+    }
+}
+
+function openMenu() {
+    document.body.classList.add('menu-expanded')
+}
+
+function closeMenu() {
+    document.body.classList.remove('menu-expanded')
+}
+
